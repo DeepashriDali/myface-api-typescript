5f501ecf7c41388eb80c315adc82c5844a91e42a
Sam James <Sam.James@softwire.com>
2022-02-09 15:53:31 +0000

    made nav responsive

--- public/src/src/Components/navbar/Navbar.jsx
+++ public/src/src/Components/navbar/Navbar.jsx
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import {Link} from "react-router-dom"
 import './NavBar.scss'
 
 export function Navbar (){
+const [clicked, setClick] = useState(false);
 
-    return <nav>
+function toggleClick(){
+    setClick(!clicked)
+}
+
+let className = clicked ? 'side-nav clicked' : 'side-nav'
+
+
+    return <div className="head">
         <h4 className='nav-link logo'>MyFace</h4>
-        <Link className='nav-link' to = "/home">Home</Link>
-        <Link className='nav-link' to = "/posts">Posts</Link>
-        <Link className='nav-link' to = "/users">Users</Link>
-    </nav>
+        <button onClick={toggleClick}>Menu</button>
+        <nav className={className}>
+            <Link className='nav-link' to="/home">Home</Link>
+            <Link className='nav-link' to="/posts">Posts</Link>
+            <Link className='nav-link' to="/users">Users</Link>
+            <Link className='nav-link' to="/users">+ New User</Link>
+        </nav>
+    </div>
 
 }
