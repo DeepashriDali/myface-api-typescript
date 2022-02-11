06db485da37bc7d2f6982a2fb2eef9c9d9250c96
Sam James <Sam.James@softwire.com>
2022-02-11 12:42:04 +0000

    merged changes

--- public/src/src/App.js
+++ public/src/src/App.js
@@ -1,6 +1,7 @@
 import { PostListPage } from './Components/posts/postListPage/PostListPage';
 import { UserListPage } from './Components/users/userListPage/UserListPage';
 import { UserDetailsPage } from './Components/users/userDetailsPage/UserDetailsPage';
+import { CreatePostPage } from './Components/posts/createPostPage/CreatePostPage';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Navbar } from './Components/navbar/Navbar';
 import './App.scss';
@@ -16,6 +17,7 @@ function App() {
         <Routes>
           <Route path = "/" element = {<PostListPage/>}/>
           <Route path="/posts" element={<PostListPage/>} />
+          <Route path="/posts/create" element={<CreatePostPage/>} />
           <Route path="/users" element={<UserListPage/>} />
           <Route path="/users/:userId" element={<UserDetailsPage/>}/>
           <Route path="/users/create" element={<CreateUserPage/>}/>
